24324836970bbb0fd75024454a21b73754f251d1
Nakul Mishra <95365344+NakulMishra@users.noreply.github.com>
2022-11-09 23:48:52 +0530

    Add files via upload

---
+++ Passgen.js
@@ -0,0 +1,17 @@
+var password = document.getElementById("password");
+function generatepassword(){
+    var chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&:;<>{}[]*()";
+    var plength = 20;
+    var password = "";
+    for(var i=0;i<plength;i++){
+        var randomnumber = Math.floor(Math.random() * chars.length);
+        password += chars.substring(randomnumber, randomnumber + 1);
+    }
+    document.getElementById("password").value = password;
+} 
+function copytext(){
+    var copyText = document.getElementById("password");
+    copyText.select();
+    copyText.setSelectionRange(0, 999);
+    document.execCommand("copy");
+}
